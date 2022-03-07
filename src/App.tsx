ff3496a49ebd47880c7445c10d483de2faf7fd1e
Andy Hu <30693884+n1203@users.noreply.github.com>
2022-03-07 10:37:26 +0800

    修复：更新函数定义不正确问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import { api, defineUpdate } from '@notionpet/sdk';
-import { useEffect, useState } from 'react'
-import { makeStyle } from './style';
-import { defaultOptions, IAppProps } from './types';
+import { api, defineUpdate } from "@notionpet/sdk";
+import { useEffect, useState } from "react";
+import { makeStyle } from "./style";
+import { defaultOptions, IAppProps } from "./types";
 
-function App({options, data}: IAppProps) {
+function App({ options, data }: IAppProps) {
   const [option, setOption] = useState(options || defaultOptions);
   const [visitorCount, setVisitorCount] = useState(data.visitorCount || 0);
   const { userName, visitor } = makeStyle(option);
   /**
    * 当数据更新时更新状态
    */
-  defineUpdate(({ data }: any) => setOption(data));
+  defineUpdate(({ options }: any) => setOption(options));
 
   useEffect(() => {
     setVisitorCount(visitorCount + 1);
     api.update({ visitorCount: visitorCount + 1 });
   }, []);
 
-  return <div>
-    👏 欢迎来到 <span style={userName}>{option.userName}</span>{" "}
-    的小屋，你是本小屋第 <span style={visitor}>{visitorCount}</span> 位访客
-  </div>
+  return (
+    <div>
+      👏 欢迎来到 <span style={userName}>{option.userName}</span>{" "}
+      的小屋，你是本小屋第 <span style={visitor}>{visitorCount}</span> 位访客
+    </div>
+  );
 }
 
-export default App
-
+export default App;
